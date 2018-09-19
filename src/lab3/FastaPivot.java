14614876500e6aead05af50cb364f0dcbabc8c8e
reeli <reeli@DESKTOP-L7QHBOG>
2018-09-19 16:00:25 -0400

    Almost there.

--- src/lab3/FastaPivot.java
+++ src/lab3/FastaPivot.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 
 
+
 public class FastaPivot
 {
 
@@ -22,6 +23,20 @@ public class FastaPivot
 			numG.add(s.length() - s.replace("G", "").length());
 			numT.add(s.length() - s.replace("T", "").length());
 		}
+	}
+	
+	public static void resultsOut(String[] seqName, String[] sequence, List<Integer> a, List<Integer> c, List<Integer> g, List<Integer> t) throws Exception
+	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File("PivotOutput.txt")));
+		writer.write("sequenceID\tnumA\tnumC\tnumG\tnumT\tsequence");
+		writer.newLine();
+		
+		for (int i = 0; i < seqName.length; i++)
+		{
+			writer.write(seqName[i]+"\t"+a.get(i)+"\t"+c.get(i)+"\t"+g.get(i)+"\t"+t.get(i)+"\t"+sequence[i]);
+			writer.newLine();
+		}
+		writer.close();
 	}
 	
 	public static void main(String[] args) throws Exception
@@ -47,6 +62,7 @@ public class FastaPivot
 			if (nextLine.startsWith(">"))
 			{
 				numSeq++;
+				nextLine = nextLine.replace("> ", "");
 				seqId.add(nextLine);
 			}
 			else
@@ -67,12 +83,26 @@ public class FastaPivot
 		System.out.println(seqId);  // for testing
 		System.out.println(sequence);   // for testing
 		String[] sequenceArray = new String[sequence.size()];
+		String[] seqIdArray = new String[seqId.size()];
+//		Integer[] numAArray = new Integer[numA.size()];
+//		String[] numCArray = new String[numC.size()];
+//		String[] numGArray = new String[numG.size()];
+//		String[] numTArray = new String[numT.size()];
 		sequenceArray = sequence.toArray(sequenceArray);
+		seqIdArray = seqId.toArray(seqIdArray);
+//		numAArray = numA.toArray(new Integer[numAArray.length]);
+//		numCArray = numC.toArray(numCArray);
+//		numGArray = numG.toArray(numGArray);
+//		numTArray = numT.toArray(numTArray);
+
+
+		
 		countAcgt(sequenceArray);
 		System.out.println(numA);	// for testing
 		System.out.println(numC);	// for testing
 		System.out.println(numG);	// for testing
 		System.out.println(numT);	// for testing  
+		resultsOut(seqIdArray, sequenceArray, numA, numC, numG, numT);
 	}
 
 }
