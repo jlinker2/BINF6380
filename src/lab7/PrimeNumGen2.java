e0eba71ffa1c6f20e4f0a5666756082ee35873bd
reeli <reeli@DESKTOP-L7QHBOG>
2018-11-15 05:01:01 -0500

    v2

--- src/lab7/PrimeNumGen2.java
+++ src/lab7/PrimeNumGen2.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.swing.JButton;
@@ -16,11 +17,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
-
-
 public class PrimeNumGen2 extends JFrame
 {
-	
 
 	private static final long serialVersionUID = 1L;
 	private final JTextArea aTextField = new JTextArea();
@@ -31,18 +29,19 @@ public class PrimeNumGen2 extends JFrame
 	private List<Integer> list = Collections.synchronizedList(new ArrayList<Integer>());
 	private final int NUMBER_OF_PROCESSORS = 4;
 	private volatile AtomicInteger count = new AtomicInteger(0);
-	
+	private volatile AtomicBoolean done = new AtomicBoolean(false);
+
 	public static void main(String[] args)
-	
+
 	{
 		PrimeNumGen2 png = new PrimeNumGen2("Primer Number Generator");
-		
+
 		// don't add the action listener from the constructor
 		png.addActionListeners();
 		png.setVisible(true);
-		
+
 	}
-	
+
 	private PrimeNumGen2(String title)
 	{
 		super(title);
@@ -51,14 +50,14 @@ public class PrimeNumGen2 extends JFrame
 		aTextField.setEditable(false);
 		setSize(400, 200);
 		setLocationRelativeTo(null);
-		//kill java VM on exit
+		// kill java VM on exit
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(new BorderLayout());
-		getContentPane().add(primeButton,  BorderLayout.SOUTH);
-		getContentPane().add(cancelButton,  BorderLayout.EAST);
-		getContentPane().add( new JScrollPane(aTextField),  BorderLayout.CENTER);
+		getContentPane().add(primeButton, BorderLayout.SOUTH);
+		getContentPane().add(cancelButton, BorderLayout.EAST);
+		getContentPane().add(new JScrollPane(aTextField), BorderLayout.CENTER);
 	}
-	
+
 	private class CancelOption implements ActionListener
 	{
 		public void actionPerformed(ActionEvent arg0)
@@ -66,129 +65,175 @@ public class PrimeNumGen2 extends JFrame
 			cancel = true;
 		}
 	}
-	
+
 	private void addActionListeners()
 	{
 		cancelButton.addActionListener(new CancelOption());
-	
-		primeButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e)
+
+		primeButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+
+				String num = JOptionPane.showInputDialog("Enter a large integer");
+				Integer max = null;
+				final long startTime;
+
+				try
 				{
-					
-					String num = JOptionPane.showInputDialog("Enter a large integer");
-					Integer max =null;
-					
-					try
+					max = Integer.parseInt(num);
+				} catch (Exception ex)
+				{
+					JOptionPane.showMessageDialog(thisFrame, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+					ex.printStackTrace();
+				}
+
+				if (max != null)
+				{
+					aTextField.setText("");
+					primeButton.setEnabled(false);
+					cancelButton.setEnabled(true);
+					cancel = false;
+					startTime = System.currentTimeMillis();
+
+					// start update thread here
+					new Thread(new UpdateText(startTime, max)).start();
+
+					//start prime threads
+					Semaphore s = new Semaphore(NUMBER_OF_PROCESSORS);
+					for (int x = 0; x < NUMBER_OF_PROCESSORS; x++)
 					{
-						max = Integer.parseInt(num);
+						s.tryAcquire();
+						new Thread(new UserInput(max, s)).start();
+
 					}
-					catch(Exception ex)
+					//gather up licenses 
+					for (int i = 0; i < NUMBER_OF_PROCESSORS; i++)
 					{
-						JOptionPane.showMessageDialog(
-								thisFrame, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-						ex.printStackTrace();
-					}
-					
-					if( max != null)
-					{
-						aTextField.setText("");
-						primeButton.setEnabled(false);
-						cancelButton.setEnabled(true);
-						cancel = false;
-						Semaphore s = new Semaphore(NUMBER_OF_PROCESSORS);
-						for (int x = 0; x < NUMBER_OF_PROCESSORS; x++)
+						try
 						{
-							s.tryAcquire();
-							new Thread(new UserInput(max, s)).start();
-//							System.out.println("thread " + x + " " + (System.currentTimeMillis() - startTime) / 1000f);
+							s.acquire();
+
+						} catch (InterruptedException e1)
+						{
+
+							e1.printStackTrace();
+							System.out.println("I tried to aquire and failed.");
 						}
 
 					}
-				}});
-		}
-	
-	private boolean isPrime( int i)
-	{
-		for( int x=2; x < i -1; x++)
-			if( i % x == 0  )
-				return false;
-		
-		return true;
-	}
-	
-	private class UserInput implements Runnable
-	{
-		private final int max;
-		private final long startTime;
-		private final Semaphore sem;
-		
-		private UserInput(int num, Semaphore sem)
-		{
-			this.max = num;
-			this.startTime = System.currentTimeMillis();
-			this.sem = sem;
-		}
-		
-		public void run()
-		{
-			long lastUpdate = System.currentTimeMillis();
-			int threadID = count.getAndIncrement();
-			
-			for (int i = threadID; i < max && ! cancel; i = i + NUMBER_OF_PROCESSORS) 
-			{
-				if( isPrime(i))
-				{
-					list.add(i);
-						
-					if( System.currentTimeMillis() - lastUpdate > 500)
-					{
-						float time = (System.currentTimeMillis() -startTime )/1000f;
-						final String outString= "Found " + list.size() + " in " + i + " of " + max + " " 
-									+ time + " seconds ";
-						
-						SwingUtilities.invokeLater( new Runnable()
-						{
-							@Override
-							public void run()
-							{
-								aTextField.setText(outString);
-							}
-						});
-						
-						lastUpdate = System.currentTimeMillis();	
-					}
+					
+					//tell the updater that we're done
+					done = new AtomicBoolean(true);
+					//tell the programmer that we're done
+					System.out.println(done);
+
 				}
 			}
-			
+		});
+	}
+
+	private boolean isPrime(int i)
+	{
+		for (int x = 2; x < i - 1; x++)
+			if (i % x == 0)
+				return false;
+
+		return true;
+	}
+
+	private class UpdateText implements Runnable
+	{
+
+		private final long startTime;
+		long lastUpdate = System.currentTimeMillis();
+		private final int max;
+
+		private UpdateText(long startTime, int max)
+		{
+			this.startTime = startTime;
+			this.max = max;
+		}
+
+		public void run()
+		{
+
+			while (!done.get())
+			{
+				if (System.currentTimeMillis() - lastUpdate > 500)
+				{
+					float time = (System.currentTimeMillis() - startTime) / 1000f;
+					final String outString = "Found " + list.size() + " of " + max + " " + time + " seconds ";
+					System.out.println("while loop");
+					SwingUtilities.invokeLater( new Runnable()
+					{
+						@Override
+						public void run()
+						{
+							aTextField.setText(outString);
+						}
+					});
+
+					lastUpdate = System.currentTimeMillis();
+				}
+			}
+
 			final StringBuffer buff = new StringBuffer();
-			
-			for( Integer i2 : list)
+
+			for (Integer i2 : list)
 				buff.append(i2 + "\n");
-			
-			if( cancel)
+
+			if (cancel)
 				buff.append("cancelled\n");
-			
-			float time = (System.currentTimeMillis() - startTime )/1000f;
-			buff.append("Time = " + time + " seconds " );
-			
-			SwingUtilities.invokeLater( new Runnable()
+
+			float time = (System.currentTimeMillis() - startTime) / 1000f;
+			buff.append("Time = " + time + " seconds ");
+
+			SwingUtilities.invokeLater(new Runnable()
 			{
 				@Override
 				public void run()
 				{
-					
+
 					cancel = false;
 					primeButton.setEnabled(true);
 					cancelButton.setEnabled(false);
-					aTextField.setText( (cancel ? "cancelled " : "") +  buff.toString());
-					
+					aTextField.setText((cancel ? "cancelled " : "") + buff.toString());
+
 				}
 			});
+		}
+	}
+
+	private class UserInput implements Runnable
+	{
+		private final int max;
+		private final Semaphore sem;
+
+		private UserInput(int num, Semaphore sem)
+		{
+			this.max = num;
+			this.sem = sem;
+		}
+
+		public void run()
+		{
+
+			int threadID = count.getAndIncrement();
+
+			for (int i = threadID; i < max && !cancel; i = i + NUMBER_OF_PROCESSORS)
+			{
+				// done.set(false);
+				if (isPrime(i))
+				{
+					list.add(i);
+
+				}
+			}
+
 			sem.release();
-			
-			
-			
+
 		}// end run
-		
-	}  // end UserInput
+
+	} // end UserInput
 }
